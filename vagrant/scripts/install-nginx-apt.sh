a3fae33608f6a6811f283f558e20772ca1a493b3
Dudu <edu.chaos@gmail.com>
2016-08-18 13:13:38 -0300

    adding gzip compressing fix nginx conf

--- vagrant/scripts/install-nginx-apt.sh
+++ vagrant/scripts/install-nginx-apt.sh
@@ -23,12 +23,12 @@ server {
 
     root /webapp/public;
 
-    try_files $uri/index.html $uri @app;
+    try_files \$uri/index.html \$uri @app;
 
     location @app {
         proxy_pass http://app;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $http_host;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$http_host;
         proxy_redirect off;
     }
 
