2c1945aacc3e47dd415cc7473bb5202fc9259918
Dudu <edu.chaos@gmail.com>
2016-08-29 17:02:27 -0300

    heroku db

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,8 @@ app_dir = File.expand_path(File.dirname(__FILE__) + '/..')
 working_directory "#{app_dir}"
 
 # Unicorn PID file location
-pid "/var/run/shared/easy-lotto-server.pid"
+#pid "/var/run/shared/easy-lotto-server.pid"
+pid "#{app_dir}/tmp/easy-lotto-server.pid"
 
 # Path to logs
 stderr_path "#{app_dir}/log/unicorn.stdout.log"
@@ -15,7 +16,7 @@ stdout_path "#{app_dir}/log/unicorn.stderr.log"
 # listen "0.0.0.0:8080", :tcp_nopush => true
 
 # Number of processes
-worker_processes (ENV['WORKERS'] || 1).to_i
+worker_processes 1
 
 # Time-out
 timeout 30
